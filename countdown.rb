ab8961305f96951a99586a1ad4c0d1a34af089ea
Carrie Savinell <carrielane89@gmail.com>
2018-04-21 17:50:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    countdown_with_sleep(number)
+  end 
 end
+
+
+puts "HAPPY NEW YEAR!"
+#end
+
+#def countdown_with_sleep(1.second)
+#end  
